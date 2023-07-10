e4d9a34d9e38a612ca1772423e25a5910c9e52ff
Alex Wang <wangpengchengnj@gmail.com>
2023-07-10 22:05:21 +1000

    Add Footer

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import PrimaryInfoPanel from "@/components/infoPanel/PrimaryInfoPanel";
 import SecondaryInfoPanel from "@/components/infoPanel/SecondaryInfoPanel";
 import { Icon } from '@iconify/react';
 import { useEffect, useState } from "react";
-
+import Footer from "@/components/Footer";
 
 export default function Home() {
 
@@ -24,24 +24,33 @@ export default function Home() {
   };
 
   return (
-    <main className='h-screen md:min-h-screen xl:h-screen w-screen overflow-hidden md:overflow-auto flex justify-center items-center bg-cover bg-center' style={{ backgroundImage: "url(/images/background.jpg)" }}>
+    <main className='relative h-screen md:min-h-screen xl:h-screen w-screen overflow-hidden md:overflow-auto flex flex-col justify-center items-center bg-cover bg-center' style={{ backgroundImage: "url(/images/background.jpg)" }}
+    >
       <section className='hidden md:grid md:gap-16 md:py-20 xl:grid xl:grid-cols-2 xl:gap-10 xl:py-0 2xl:gap-28'>
         <PrimaryInfoPanel />
         <SecondaryInfoPanel />
         <SecondaryInfoPanel />
         <SecondaryInfoPanel />
       </section>
-      <section className="h-full pt-10 md:hidden">
-        <div className="">
-          {mobileClockList[mobileClockIndex]}
+      <div className="hidden md:block absolute bottom-6">
+        <Footer />
+      </div>
+      <section className="h-full flex flex-col justify-center items-center gap-20 md:hidden">
+        <div>
+          <div className="">
+            {mobileClockList[mobileClockIndex]}
+          </div>
+          <div className="flex flex-row justify-center items-center mt-6 gap-12">
+            <div className="p-1 rounded-full border border-[#757575] cursor-pointer " onClick={handleLeftClick}>
+              <Icon icon="uiw:left" color="#757575" width="30" />
+            </div>
+            <div className="p-1 rounded-full border border-[#757575] cursor-pointer" onClick={handleRightClick}>
+              <Icon icon="uiw:right" color="#757575" width="30" />
+            </div>
+          </div>
         </div>
-        <div className="flex flex-row justify-center items-center mt-6 gap-12">
-          <div className="p-1 rounded-full border border-[#757575] cursor-pointer " onClick={handleLeftClick}>
-            <Icon icon="uiw:left" color="#757575" width="30" />
-          </div>
-          <div className="p-1 rounded-full border border-[#757575] cursor-pointer" onClick={handleRightClick}>
-            <Icon icon="uiw:right" color="#757575" width="30" />
-          </div>
+        <div className="flex justify-center items-center ">
+          <Footer />
         </div>
       </section>
     </main>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import { Icon } from "@iconify/react";
+
+export default function Footer(props) {
+  return (
+    <section className="flex flex-row items-center gap-8">
+      <span className="text-xs tracking-tighter pl-4 text-violet-300">Designed and Coded by Alex Wang</span>
+      <div className="flex pr-4 gap-2">
+        <a href="https://github.com/MarkLumenAW/clock-weather" target="_blank" rel="noopener noreferrer">
+          <Icon icon="mdi:github" color="#c4b5fd" width="1rem" />
+        </a>
+        <a href="https://www.linkedin.com/in/pengcheng-wang-0412b0151/" target="_blank" rel="noopener noreferrer">
+          <Icon icon="mdi:linkedin" color="#c4b5fd" width="1rem" />
+        </a>
+      </div>
+    </section>
+  );
+};
