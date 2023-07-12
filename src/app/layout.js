385a4954b32caf3053b7063199b39788c8fa599a
Alex Wang <wangpengchengnj@gmail.com>
2023-07-12 17:00:57 +1000

    Add Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Clock & Weather',
   description: 'Clock & Weather',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
